af85f0dd83a28ac1a4a94f8fe9934c932f43eb47
abdoulkarimou <abdoulkarimmahamadou4@gmail.com>
2026-09-02 18:38:30 +0100

    feat: add backup.sh

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+set -uo pipefail
+
+ if [ $# -eq 0 ]; then
+    echo "You don't give any file path to backup. Using default path: ./sensitive-data"
+    filePath="./sensitive-data"
+    else
+    filePath="$1"
+fi
+
+if [ ! -f "$filePath" ] && [ ! -d "$filePath" ]; then
+    echo "Error: '$filePath' does not exist or is not a valid file/directory."
+    exit 1
+fi
+
+
+if [ ! -d "./backups" ]; then
+    mkdir "./backups"
+fi
+
+date=$(date +%Y-%m-%d)
+if tar -czf "./backups/backup_zip_file_${date}_archive.tar.gz" "$filePath"; then
+    echo "Backup successful."
+else
+    echo "Backup failed."
+    exit 1
+fi
